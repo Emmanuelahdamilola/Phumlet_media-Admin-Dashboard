75a5d1cc0415aaaa935e1594160adec4d958ee7a
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2025-01-19 23:07:46 +0100

    ChartJs for the dashboard implemented

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ hamburger.addEventListener("click", function(){
     toggler.classList.toggle("bx-chevrons-right");
     toggler.classList.toggle("bx-chevrons-left");
 });
+
+
+// chart js function
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
